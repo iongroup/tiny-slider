684e21319940f1bb9adda635bb2ef33a020e2354
Chirag Khanna <chirag.khanna@iongroup.com>
2022-05-27 13:45:50 +0530

    Change the build from cjs to umd Remoed multiple instances of preventScrollOnTouch from d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,11 @@
-import { babel } from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
 
 const config = {
   input: 'src/tiny-slider.js',
   output: {
     file: 'dist/tiny-slider.js',
-    format: 'cjs'
-  },
-  plugins: [
-    commonjs({ defaultIsModuleExports: false }),
-    babel({ babelHelpers: 'bundled' })
-  ],
+    format: 'umd',
+    name: 'tns'
+  }
 };
 
 export default config;
